e1d8beb10360aa4b1ab566f48c8a3386f03e40b3
Emma Immink <Emma@wcw-stud-145-109-15-8.wireless.uva.nl>
2016-04-13 14:55:40 +0200

    alle knoppen werken, goede layout. Reset to constraints duurt erg lang

--- Mr.Potatohead/ViewController.swift
+++ Mr.Potatohead/ViewController.swift
@@ -10,8 +10,39 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var Arms: UIImageView!
+    @IBOutlet weak var Ears: UIImageView!
+    @IBOutlet weak var Eyebrows: UIImageView!
+    @IBOutlet weak var Eyes: UIImageView!
+    @IBOutlet weak var Glasses: UIImageView!
+    @IBOutlet weak var Hat: UIImageView!
+    @IBOutlet weak var Mouth: UIImageView!
+    @IBOutlet weak var Moustache: UIImageView!
+    @IBOutlet weak var Nose: UIImageView!
+    @IBOutlet weak var Shoes: UIImageView!
+    
     @IBAction func SwitchOn(sender: AnyObject) {
         print("aan of uit \(sender.tag)")
+        if (sender.tag == 0){
+            Arms.hidden = !Arms.hidden}
+        if (sender.tag == 1){
+            Ears.hidden = !Ears.hidden}
+        if (sender.tag == 2){
+            Eyebrows.hidden = !Eyebrows.hidden}
+        if (sender.tag == 3){
+            Eyes.hidden = !Eyes.hidden}
+        if (sender.tag == 4){
+            Glasses.hidden = !Glasses.hidden}
+        if (sender.tag == 5){
+            Hat.hidden = !Hat.hidden}
+        if (sender.tag == 6){
+            Mouth.hidden = !Mouth.hidden}
+        if (sender.tag == 7){
+            Moustache.hidden = !Moustache.hidden}
+        if (sender.tag == 8){
+            Nose.hidden = !Nose.hidden}
+        if (sender.tag == 9){
+            Shoes.hidden = !Shoes.hidden}
     }
     
     @IBOutlet weak var NamePotato: UILabel!
